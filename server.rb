53ac746a593ccc9ee9d793c3257f2edd89c59812
Jen Villaganas <jenvillaganas@gmail.com>
2022-01-31 13:34:10 +0800

    Use 3 threads at a time

--- server.rb
+++ server.rb
@@ -32,29 +32,34 @@ EM.run {
           threads = []
           success = []
           failed = []
-          recipients.each do |recipient|
-            threads << Thread.new do
-              stdin, stdout, stderr, wait_thr = Open3.popen3(
-                'spl-token', 'transfer', token_address, '1', recipient, '--allow-unfunded-recipient', '--fund-recipient')
+          recipients.each_slice(3).to_a.each do |recipient_arr|
+            small_threads = []
+            recipient_arr.each do |recipient|
+              small_threads << Thread.new do
+                stdin, stdout, stderr, wait_thr = Open3.popen3(
+                  'spl-token', 'transfer', token_address, '1', recipient, '--allow-unfunded-recipient', '--fund-recipient')
 
-              log = stdout.read
-              err = stderr.read
-              puts log
-              puts err
-              split_with_signature = log.split("Signature: ")
-              if split_with_signature.count > 1
-                signature = split_with_signature.last.strip
-                # ws.send({status: 200, signature: signature}.to_json)
-                success << {recipient: recipient, signature: signature}
-              elsif err
-                failed << {recipient: recipient, log: err}
-              else
-                failed << {recipient: recipient, log: log + err}
+                log = stdout.read
+                err = stderr.read
+                puts log
+                puts err
+                split_with_signature = log.split("Signature: ")
+                if split_with_signature.count > 1
+                  signature = split_with_signature.last.strip
+                  # ws.send({status: 200, signature: signature}.to_json)
+                  success << {recipient: recipient, signature: signature}
+                elsif err
+                  failed << {recipient: recipient, log: err}
+                else
+                  failed << {recipient: recipient, log: log + err}
+                end
               end
             end
+
+            small_threads.each(&:join)
+            puts "Completed: #{recipient_arr}"
           end
 
-          threads.each(&:join)
           ws.send({status: 200, signature: {success: success, failed: failed}}.to_json)
         else
           ws.send("failed")
